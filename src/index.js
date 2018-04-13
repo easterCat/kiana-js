8717cc3c632dcb78ed9141b866a66c8df456daea
easterCat <fuhuodemao@163.com>
2018-04-13 16:20:10 +0800

    '修改'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,76 @@
 import './index.html';
-import _ from './kiana/Kiana';
+import {
+    difference,
+    flatten,
+    findIndex,
+    findLastIndex,
+    sortedIndex,
+    indexOf,
+    lastIndexOf,
+    unique,
+    without,
+    max,
+    min
+} from './kiana/arrays';
+
+import {
+    keys,
+    values,
+    extend,
+    isElement,
+    isArray,
+    isObject,
+    isPlainObject,
+    isWindow,
+    isFunction,
+    isDate,
+    isError,
+    isString,
+    isArguments,
+    isNumber,
+    isRegExp,
+    isEmpty,
+    pick
+} from './kiana/arrays';
+
+import {
+    debounce,
+    throttle
+} from './kiana/arrays';
+
+
+let obj = {
+    difference,
+    flatten,
+    findIndex,
+    findLastIndex,
+    sortedIndex,
+    indexOf,
+    lastIndexOf,
+    unique,
+    without,
+    max,
+    min,
+    keys,
+    values,
+    extend,
+    isElement,
+    isArray,
+    isObject,
+    isPlainObject,
+    isWindow,
+    isFunction,
+    isDate,
+    isError,
+    isString,
+    isArguments,
+    isNumber,
+    isRegExp,
+    isEmpty,
+    pick,
+    debounce,
+    throttle
+}
+
+export default obj;
 
