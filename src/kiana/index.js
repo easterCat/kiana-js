c27a201a25be2c63b4f9abcb28d4a094686a736f
easterCat <fuhuodemao@163.com>
2018-04-17 18:01:59 +0800

    'index'

---
+++ src/kiana/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by easterCat on 2018/4/17.
+ */
+import * as arr from './arrays'
+import * as func from './functions'
+import * as obj from './objects'
+
+let kiana = Object.assign({}, arr, func, obj)
+
+export default kiana
