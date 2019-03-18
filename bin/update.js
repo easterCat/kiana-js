8ee40d15e5b4426450d592105a02e1fc53401827
easterCat <fuhuodemao@163.com>
2019-03-18 18:25:27 +0800

    快速提交=>2019-3-18 18:25:27

--- bin/update.js
+++ bin/update.js
@@ -1,9 +1,11 @@
 const { exec } = require("child_process");
-const stamp = new Date().toLocaleDateString();
+const stamp = `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`;
 
 console.log(stamp);
 
-const command = `git add . && git commit -m '快速提交=>${stamp}' && git push -u origin master`;
+const command = `git add . && git commit -m "快速提交=>${stamp}" && git push -u origin master`;
+
+console.log(command);
 
 exec(command, function(err) {
     err && console.log(err);
