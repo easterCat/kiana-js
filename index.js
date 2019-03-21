2f7e551ac657c6113e91d7c1de442a15900df11b
easterCat <fuhuodemao@163.com>
2019-03-21 13:44:17 +0800

    快速提交=>2019-3-21 13:44:17

--- index.js
+++ index.js
@@ -2,19 +2,30 @@
  * Created by easterCat on 2018/4/13.
  */
 
+//数组
 import * as arr from "./src/arrays/index";
+//函数
 import * as func from "./src/functions/index";
+//对象
 import * as obj from "./src/objects/index";
+//cookie操作
 import * as cookie from "./src/cookie/index";
+//时间方法
 import * as time from "./src/time/index";
+//浏览器方法
 import * as browser from "./src/browser/browser";
+//设备判断
 import * as mobile from "./src/browser/mobile";
+//本地存储
 import * as localStorage from "./src/localStorage";
+//会话存储
 import * as sessionStorage from "./src/sessionStorage";
+//日期方法
 import * as date from "./src/date/date";
-import {fileDownload} from "./src/dom/download";
+//工具方法
+import * as utils from "./src/dom/download";
 
-const kiana = Object.assign(
+module.exports = Object.assign(
     {},
     arr,
     func,
@@ -26,7 +37,5 @@ const kiana = Object.assign(
     localStorage,
     sessionStorage,
     date,
-    {fileDownload},
+    utils,
 );
-
-export default kiana;
