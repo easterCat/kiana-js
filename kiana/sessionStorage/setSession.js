e4bddbaff9de4e047398eb7790fb3b859294556d
easterCat <fuhuodemao@163.com>
2019-04-06 21:35:57 +0800

    快速提交=>2019-4-6 21:35:57

--- kiana/sessionStorage/setSession.js
+++ kiana/sessionStorage/setSession.js
@@ -3,10 +3,15 @@
  */
 
 export function setSession(name, data) {
-  var store = sessionStorage.getItem(name);
-  if (store) {
-    console.warn(name + "=>数据在sessionStorage已存在,执行替换操作");
-    sessionStorage.removeItem(name);
-  }
-  sessionStorage.setItem(name, JSON.stringify(data));
+    var store = sessionStorage.getItem(name);
+    if (store) {
+        console.warn(name + "=>数据在sessionStorage已存在,执行替换操作");
+        sessionStorage.removeItem(name);
+    }
+
+    if (typeof data === 'object') {
+        sessionStorage.setItem(name, JSON.stringify(data));
+    } else {
+        sessionStorage.setItem(name, data);
+    }
 }
